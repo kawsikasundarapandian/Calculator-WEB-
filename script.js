80436983be93a7327944f0792f14dc4b4d9d79bd
kawsikasundarapandian <kawsikasundarapandian1@gmail.com>
2025-09-27 08:42:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let display = document.getElementById("display");
+
+function appendValue(val) {
+  display.value += val;
+}
+function clearDisplay() {
+  display.value = "";
+}
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
